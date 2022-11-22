fdf526b3e03e49972cb38653e707089c393b8638
w3projects <manzill.surolia@gmail.com>
2022-11-22 09:43:28 +0530

    List View

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] cities = {"Jaipur", "Mumbai", "Bangalore", "Noida", "Ggn"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, cities);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, "Clicked: " + cities[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
